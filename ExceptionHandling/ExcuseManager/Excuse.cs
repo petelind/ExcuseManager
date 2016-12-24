d670424343b907bb202fcbe25fbe7fe59b972687
petelind <d.petelin@icloud.com>
2016-12-24 15:36:57 +0300

    Еще одна версия: обрабатывается ошибка входа - а что если нам дали на вход неправильный файл?

---
+++ ExceptionHandling/ExcuseManager/Excuse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary; // Здесь живет бинарный форматтер
+using System.Runtime.Serialization;
+
+namespace ExcuseManager
+{
+    [Serializable] // этот ключ разрешает сериализовывать объекты этого типа
+    public class Excuse
+    {
+        public string Description;
+        public string Results;
+        public DateTime LastUsed;
+
+        public string Save (string path, DateTime date)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (Stream output = File.Create(path))
+            {
+                formatter.Serialize(output, this);
+                return Convert.ToString(File.GetLastAccessTime(path));
+            }
+            
+        }
+        public void Load(string path)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (Stream input = File.Open(path, FileMode.Open))
+            {
+                try
+                {
+                    Excuse temp = (Excuse)formatter.Deserialize(input);
+                    this.Description = temp.Description;
+                    this.Results = temp.Results;
+                    this.LastUsed = temp.LastUsed;
+                }
+                catch (SerializationException)
+                {
+                    System.Windows.Forms.MessageBox.Show("File you have chosen is not a valid EX file, choose another one!", "Invalid File!");                                  
+                }
+                           
+            }
+        }
+
+    }
+}
---
+++ ExceptionHandling/ExcuseManager/MainForm.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ExcuseManager
+{
+    public partial class MainForm : Form
+    {
+        string currentFolder = Directory.GetCurrentDirectory() + @"\Excuses";
+        Excuse CurrentExcuse;
+        bool isChanged = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            foreach (Control control in this.Controls)
+            {
+                control.Enabled = false;
+            }
+            buttonChooseFolder.Enabled = true;
+            CurrentExcuse = new Excuse();
+            CurrentExcuse.LastUsed = dateTimePicker1.Value;
+
+        }
+
+        private void buttonChooseFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowNewFolderButton = true;
+            folderBrowserDialog1.SelectedPath = currentFolder;
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (Control control in this.Controls)
+                {
+                    control.Enabled = true;
+                    currentFolder = folderBrowserDialog1.SelectedPath;
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (!isChanged)
+            {
+                MessageBox.Show("Nothing to save, enter or edit data!");
+                return;
+            }
+
+            saveFileDialog1.Title = "Save an excuse";
+            saveFileDialog1.Filter = "Excuse files | *.ex";
+            saveFileDialog1.DefaultExt = "ex";
+            saveFileDialog1.InitialDirectory = currentFolder;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                CurrentExcuse.Save(saveFileDialog1.FileName, DateTime.Now);
+                isChanged = false;
+                UpdateForm();
+                MessageBox.Show("Excuse Successfully Saved!");
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            CurrentExcuse.Description = textBox1.Text;
+            isChanged = true;
+            UpdateForm();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            CurrentExcuse.Results = textBox2.Text;
+            isChanged = true;
+            UpdateForm();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            CurrentExcuse.LastUsed = dateTimePicker1.Value;
+            isChanged = true;
+            UpdateForm();
+
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Choose excuse file to load...";
+            openFileDialog1.Filter = "Excuse files | *.ex";
+            openFileDialog1.InitialDirectory = currentFolder;
+            openFileDialog1.CheckFileExists = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                CurrentExcuse.Load(openFileDialog1.FileName);
+                UpdateForm();
+                
+            }
+
+        }
+
+        private void UpdateForm()
+        {
+            textBox1.Text = CurrentExcuse.Description;
+            textBox2.Text = CurrentExcuse.Results;
+            dateTimePicker1.Value = CurrentExcuse.LastUsed;
+            textBox3.Text = Convert.ToString(File.GetLastAccessTime(openFileDialog1.FileName));
+            if (isChanged) this.Text = "Excuse Manager - Unsaved Changes";
+            else this.Text = "Excuse Manager";
+
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            string[] excuseFiles;
+            excuseFiles = Directory.GetFiles(currentFolder, "*.ex", SearchOption.AllDirectories);
+            if (excuseFiles.Count() == 0)
+            {
+                MessageBox.Show("No excuses! Do the thing finally, or create new excuse!");
+                return;
+            }
+            else
+            {
+                Random random = new Random();
+                int numOfExcuseToLoad = random.Next(0, excuseFiles.Count());
+                string pathToExcuse = excuseFiles[numOfExcuseToLoad];
+                CurrentExcuse.Load(pathToExcuse);
+                UpdateForm();
+
+            }
+
+
+        }
+    }
+}
